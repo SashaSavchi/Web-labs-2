00604ce70e080db91ceca3a0e55061a9a6895916
Александра Цуканова <sasha.2014.21@yandex.ru>
2024-10-01 14:08:37 +0700

    Имя студента

--- app.py
+++ app.py
@@ -508,5 +508,6 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name ='Александра Цуканова'
+    return render_template('example.html', name=name)
 
