90037ab0d4228687ad719d295034ba6a9f2e6e89
Александра Цуканова <sasha.2014.21@yandex.ru>
2024-11-17 18:50:13 +0700

    Заготовка к 5-ой лабораторной

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
 @app.route("/")
 @app.route("/index")
@@ -32,6 +34,10 @@ def index():
                     <li><a href="/lab3">Третья лабораторная</a></li>
                     <li><a href="/lab4">Четвертая лабораторная</a></li>
                     <li><a href="/lab5">Пятая лабораторная</a></li>
+                    <li><a href="/lab6">Шестая лабораторная</a></li>
+                    <li><a href="/lab7">Седьмая лабораторная</a></li>
+                    <li><a href="/lab8">Восьмая лабораторная</a></li>
+                    <li><a href="/lab9">Девятая лабораторная</a></li>
                 </ul>
             <footer>
                 Цуканова Александра Руслановна, группа ФБИ-21, курс 3, год 2024
