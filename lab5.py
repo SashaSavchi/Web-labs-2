7e3e77f911772a4499d1908c935974968bcca58c
Александра Цуканова <sasha.2014.21@yandex.ru>
2024-11-18 21:24:27 +0700

    Создание статьи

--- lab5.py
+++ lab5.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, redirect, render_template, request, session
+from flask import Blueprint, redirect, render_template, request, session 
 lab5 = Blueprint('lab5',__name__)
-import psycopg2
-from psycopg2.extras import RealDictCursor
+import psycopg2 
+from psycopg2.extras import RealDictCursor 
 from werkzeug.security import check_password_hash, generate_password_hash
 
 @lab5.route('/lab5/')
@@ -81,3 +81,27 @@ def login():
     session['login'] = login
     db_close(conn, cur)
     return render_template('lab5/success_login.html', login = login)
+
+
+@lab5.route('/lab5/create', methods = ['GET', 'POST'])
+def create():
+    login=session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+
+    if request.method == 'GET':
+        return render_template('lab5/create_article.html')
+
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    conn, cur = db_connect()
+
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
+    user_id = cur.fetchone()["id"]
+
+    cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
+                VALUES ({user_id}, '{title}', '{article_text}');")
+
+    db_close(conn, cur)
+    return redirect('/lab5')
