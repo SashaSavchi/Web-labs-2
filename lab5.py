fa0355a7ed8df00a7bbcb74edbe736fd448c182f
Александра Цуканова <sasha.2014.21@yandex.ru>
2024-11-18 22:31:41 +0700

    Подключение бд SQLite

--- lab5.py
+++ lab5.py
@@ -3,6 +3,8 @@ lab5 = Blueprint('lab5',__name__)
 import psycopg2 
 from psycopg2.extras import RealDictCursor 
 from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+from os import path
 
 @lab5.route('/lab5/')
 def lab():
@@ -10,13 +12,20 @@ def lab():
 
 
 def db_connect():
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'alexandra_tsukanova_knowledge_base',
-        user = 'alexandra_tsukanova_knowledge_base',
-        password = '21581a'
-    )
-    cur = conn.cursor(cursor_factory = RealDictCursor)
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host = '127.0.0.1',
+            database = 'alexandra_tsukanova_knowledge_base',
+            user = 'alexandra_tsukanova_knowledge_base',
+            password = '21581a'
+        )
+        cur = conn.cursor(cursor_factory = RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
 
     return conn, cur 
     
