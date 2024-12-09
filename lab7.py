898a22e065381547e274fc0c02afaeeff2ee6961
Александра Цуканова <sasha.2014.21@yandex.ru>
2024-12-09 20:24:23 +0700

    Редактирование выбранного фильма по rest

--- lab7.py
+++ lab7.py
@@ -89,3 +89,13 @@ def del_film(id):
         return {"error": "Film not found"}, 404
     del films[id]
     return '', 204
+
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    if id < 0 or id >= len(films):
+        return {"error": "Film not found"}, 404
+    film = request.get_json()
+    films[id] = film
+    return films[id]
+
