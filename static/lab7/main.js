04a928fb23261e76a075351809752fe45be1cc86
Александра Цуканова <sasha.2014.21@yandex.ru>
2024-12-12 11:00:29 +0700

    Проверка описания

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -71,10 +71,18 @@ function sendFilm() {
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(film)
     })
-    .then(function() {
-        fillFilmList();
-        hideModal();
+    .then(function(resp) {
+        if (resp.ok) {
+            fillFilmList();
+            hideModal();
+            return{};
+        }
+        return resp.json();
     })
+    then(function(errors){
+        if(errors.description)
+            document.getElementById('description-error').innerText = errors.description;
+    });
 }
 
 function editFilm(id) {
@@ -94,6 +102,7 @@ function editFilm(id) {
 
 function showModal() {
     document.querySelector('div.modal').style.display = 'block';
+    document.getElementById('description-error').innerText = '';
 }
 
 function hideModal() {
