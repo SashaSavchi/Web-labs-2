c22929df944a35a0a063a45f6095c949461d883c
Александра Цуканова <sasha.2014.21@yandex.ru>
2024-12-17 01:35:11 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, render_template, request, session
 from db import db
 from db.models import users, articles
+from flask_login import login_user, login_required, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 
 lab8 = Blueprint('lab8',__name__)
@@ -8,6 +9,7 @@ lab8 = Blueprint('lab8',__name__)
 @lab8.route('/lab8/')
 def lab():
     return render_template('lab8/lab8.html', login=session.get('login'))
+
 
 @lab8.route('/lab8/register', methods = ['GET', 'POST'])
 def register():
@@ -32,4 +34,36 @@ def register():
     new_user = users(login = login_form, password = password_hash)
     db.session.add(new_user)
     db.session.commit()
-    return redirect('/lab8')
+    return render_template('lab8/success_reg.html',login = login_form)
+
+
+@lab8.route('/lab8/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if login_form == '':
+        return render_template('lab8/login.html', 
+                               error='Заполните имя пользователя')
+    if password_form == '':
+        return render_template('lab8/login.html', 
+                               error='Заполните пароль')
+    
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember = False)
+            return render_template('lab8/success_log.html', login=login_form)
+        
+    return render_template('lab8/login.html',login = login_form, 
+                           error='Ошибка входа: логин и/или пароль неверны')
+
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "Cписок статей"
